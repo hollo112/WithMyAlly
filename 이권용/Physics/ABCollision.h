1fed7c8a9946571c8b642f2108645fbfa0a9c154
Lee Gwon Yong <manutd202307@tukorea.ac.kr>
2024-01-24 07:12:48 +0900

    충돌 전처리기 함수 추가

---
+++ 이권용/Physics/ABCollision.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+
+#include "/UE_5.3/Engine/Source/Runtime/Core/Public/CoreMinimal.h"
+
+#define CPROFILE_ABCAPSULE TEXT("ABCapusle")
+#define CPROFILE_ABTRIGGER TEXT("ABTrigger")
+#define CCHANNEL_ABACTION ECC_GameTraceChannel1
